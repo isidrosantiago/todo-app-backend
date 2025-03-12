92f73d1a4c61c8eeb37fe9dbf38eee8800f8f7ba
Isidro Santiago <santiagoissidro@gmail.com>
2025-03-12 16:24:34 -0600

    chore: update cors config

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.ORIGIN,
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
